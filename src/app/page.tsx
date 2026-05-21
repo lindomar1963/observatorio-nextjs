8d976ef64d5782ace0983ccfd84b1f5715dfb3f0
Lindomar <brito1963@gmail.com>
2026-05-21 13:18:50 -0400

    Corrige pagina inicial

---
+++ src/app/page.tsx
@@ -0,0 +1,27 @@
+import Nav from '@/components/Nav'
+import Hero from '@/components/Hero'
+import Ticker from '@/components/Ticker'
+import StatsGrid from '@/components/StatsGrid'
+import DashboardPreview from '@/components/DashboardPreview'
+import RelatoriosRecentes from '@/components/RelatoriosRecentes'
+import Parceiros from '@/components/Parceiros'
+import Footer from '@/components/Footer'
+import { getDadosDiarios } from '@/lib/queries'
+
+export const revalidate = 3600
+
+export default async function HomePage() {
+  const dados = await getDadosDiarios()
+  return (
+    <main id="main-content">
+      <Nav />
+      <Ticker atualizado_em={dados.indicadores.atualizado_em} />
+      <Hero indicadores={dados.indicadores} />
+      <StatsGrid indicadores={dados.indicadores} />
+      <DashboardPreview />
+      <RelatoriosRecentes relatorios={dados.relatorios} />
+      <Parceiros />
+      <Footer />
+    </main>
+  )
+}
